12a7d960c5150835fe423b92fea597181d04e969
seiobata <s.bautista18@yahoo.com>
2025-01-31 19:06:10 -0600

    added a rate limit to shoot

--- player.py
+++ player.py
@@ -7,6 +7,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.rate_limit = 0
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -27,10 +28,14 @@ class Player(CircleShape):
         self.position += (forward * PLAYER_SPEED * dt)
 
     def shoot(self):
+        if self.rate_limit > 0:
+            return
+        self.rate_limit = PLAYER_SHOOT_COOLDOWN
         shot = Shot(self.position.x, self.position.y)
         shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
 
     def update(self, dt):
+        self.rate_limit -= dt
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
