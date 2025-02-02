089096b2bad5a7df3014541cee37af7af0ef26d1
seiobata <s.bautista18@yahoo.com>
2025-02-01 18:55:54 -0600

    added split, now asteroids get smaller when shot

--- asteroid.py
+++ asteroid.py
@@ -1,5 +1,7 @@
 import pygame
+import random
 from circleshape import CircleShape
+from constants import *
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -10,3 +12,18 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += (self.velocity * dt)
+        
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        
+        angle = random.uniform(20, 50)
+        vector_one = self.velocity.rotate(angle)
+        vector_two = self.velocity.rotate(-angle)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+
+        asteroid_one = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid_one.velocity = vector_one * 1.2
+        asteroid_two = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid_two.velocity = vector_two * 1.2
--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def main():
             
             for shot in shots:
                 if asteroid.collision(shot):
-                    asteroid.kill()
+                    asteroid.split()
                     shot.kill()
 
         pygame.display.flip()
